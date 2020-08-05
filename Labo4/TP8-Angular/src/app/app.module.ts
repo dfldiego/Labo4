1c08def80913fad651d5aaa184514422c60b65e4
dfldiego <dfl.diego92@gmail.com>
2020-08-05 17:07:16 -0300

    bootstrap. componentes:home-dondeEstamos-productos-navbar

--- Labo4/TP8-Angular/src/app/app.module.ts
+++ Labo4/TP8-Angular/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DondeEstamosComponent } from './components/donde-estamos/donde-estamos.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DondeEstamosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
