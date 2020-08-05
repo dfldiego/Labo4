2a2c5e743157c647a45470bc783e73f91051aabc
dfldiego <dfl.diego92@gmail.com>
2020-08-05 18:11:19 -0300

    rutas agregadas:home,donde-estamos,productos

--- Labo4/TP8-Angular/src/app/app-routing.module.ts
+++ Labo4/TP8-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DondeEstamosComponent } from './components/donde-estamos/donde-estamos.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'donde-estamos', component: DondeEstamosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
