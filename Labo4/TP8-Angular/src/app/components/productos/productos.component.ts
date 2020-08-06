cd7380cc7b791e3449b3ff76284251cf8a6a97f6
dfldiego <dfl.diego92@gmail.com>
2020-08-06 14:22:23 -0300

    services. vinculando services Instrumento con el component Productos.

--- Labo4/TP8-Angular/src/app/components/productos/productos.component.ts
+++ Labo4/TP8-Angular/src/app/components/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InstrumentosService } from 'src/app/services/instrumentos.service';
 
 @Component({
   selector: 'app-productos',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serviceInstrumentos: InstrumentosService) { }
 
   ngOnInit(): void {
   }
