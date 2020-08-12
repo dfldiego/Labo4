677e2748f38048231bfb6ec5df88227e61fbf704
dfldiego <dfl.diego92@gmail.com>
2020-08-12 11:24:17 -0300

    Entidad Instrumento creada.Libreria HTTP.getAll() & getOne() en Service.

--- Labo4/TP9-Angular-Nodejs/angular/src/app/services/instrumentos.service.ts
+++ Labo4/TP9-Angular-Nodejs/angular/src/app/services/instrumentos.service.ts
@@ -1,28 +1,42 @@
 import { Injectable } from '@angular/core';
 import * as data from 'src/assets/datos/instrumentos.json';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Instrumento } from '../entities/Instrumento';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstrumentosService {
-  instrumentosFile: any = (data as any).default;
+  //instrumentosFile: any = (data as any).default;
+  public instrumentosData: Instrumento[] = [];
+  public instrumentoEncontrado: Instrumento;
 
-  constructor() {
+  constructor(public http: HttpClient) {
     console.log("Servicio Cargado!!!");
-    //console.log(this.instrumentosFile);
   }
 
   public getInstrumentos(): any[] {
-    //console.log(this.instrumentosFile.instrumentos);
-    return this.instrumentosFile.instrumentos;
+    console.log(this.instrumentosData);
+    return this.instrumentosData;
   }
 
-  public getInstrumentoXId(idx: string): any[] {
-    for (let instrumento of this.instrumentosFile.instrumentos) {
+  public getInstrumentoXId(idx: string): any {
+    for (let instrumento of this.instrumentosData) {
       if (instrumento.id == idx) {
         return instrumento;
       }
     }
+  }
+
+  //Metodo GETALL()
+  getInstrumentosFromDataBase() {
+    return this.http.get("http://localhost:3000/instrumento/").pipe(map(instrumentosData => instrumentosData));
+  }
+
+  //Metodo GETONE()
+  getInstrumentoFromDataBaseById(idx: string) {
+    return this.http.get("http://localhost:3000/instrumento/" + idx).pipe(map(instrumentoEncontrado => instrumentoEncontrado));
   }
 
 }
